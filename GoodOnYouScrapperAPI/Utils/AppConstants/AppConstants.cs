e496322964bd66a9ce89df784821569caff6fefc
Thomas Blondel <tfoenix@gmail.com>
2023-03-17 12:42:27 +0100

    [Constants] Added web site url

--- GoodOnYouScrapperAPI/Utils/AppConstants/AppConstants.cs
+++ GoodOnYouScrapperAPI/Utils/AppConstants/AppConstants.cs
@@ -2,5 +2,5 @@ namespace GoodOnYouScrapperAPI.Utils.AppConstants;
 
 public class AppConstants: IAppConstants
 {
-    
+    public static readonly string WebSiteUrl = "https://directory.goodonyou.eco/brand/";
 }

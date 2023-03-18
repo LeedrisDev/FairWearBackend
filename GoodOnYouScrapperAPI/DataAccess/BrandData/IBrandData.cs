e4fc7c796bbd44bd2cf4180a00b2e072771fa6e3
Thomas Blondel <tfoenix@gmail.com>
2023-03-19 00:21:40 +0100

    [BrandData] Created and done

--- GoodOnYouScrapperAPI/DataAccess/BrandData/IBrandData.cs
+++ GoodOnYouScrapperAPI/DataAccess/BrandData/IBrandData.cs
@@ -1,6 +1,11 @@
+using HtmlAgilityPack;
+
 namespace GoodOnYouScrapperAPI.DataAccess.BrandData;
 
 public interface IBrandData
 {
-    
+    /// <summary>Get the brand page</summary>
+    /// <param name="brandName">Name of the brand</param>
+    /// <returns>An <see cref="HtmlDocument"/> containing all information on the brand</returns>
+    public Task<HtmlDocument> GetBrandPageHtml(string brandName);
 }

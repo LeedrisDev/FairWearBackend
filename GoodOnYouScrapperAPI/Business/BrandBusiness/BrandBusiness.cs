cc78ed8a32c2a25b02f74071cb04080580f3967d
Thomas Blondel <tfoenix@gmail.com>
2023-03-19 01:31:40 +0100

    [BusinessBrand] Created class and interface

--- GoodOnYouScrapperAPI/Business/BrandBusiness/BrandBusiness.cs
+++ GoodOnYouScrapperAPI/Business/BrandBusiness/BrandBusiness.cs
@@ -1,6 +1,38 @@
+using System.Net;
+using GoodOnYouScrapperAPI.DataAccess.BrandData;
+using GoodOnYouScrapperAPI.Models;
+using HtmlAgilityPack;
+
 namespace GoodOnYouScrapperAPI.Business.BrandBusiness;
 
 public class BrandBusiness: IBrandBusiness
 {
+    private ProcessingStatusResponse<BrandModel> _processingStatusResponse;
+    private readonly IBrandData _brandData;
     
+    public BrandBusiness(IBrandData brandData)
+    {
+        _processingStatusResponse = new ProcessingStatusResponse<BrandModel>();
+        _brandData = brandData;
+    }
+    
+    public async Task<ProcessingStatusResponse<BrandModel>> GetBrandInformation(string brandName)
+    {
+        HtmlDocument htmlDocument;
+
+        try
+        {
+            htmlDocument = await _brandData.GetBrandPageHtml(brandName);
+        }
+        catch (Exception e)
+        {
+            _processingStatusResponse.Status = HttpStatusCode.NotFound;
+            _processingStatusResponse.MessageObject.Message = e.Message;
+            return _processingStatusResponse;
+        }
+        
+        // TODO: Implement the rest of the logic
+
+        throw new NotImplementedException();
+    }
 }
--- GoodOnYouScrapperAPI/Business/BrandBusiness/IBrandBusiness.cs
+++ GoodOnYouScrapperAPI/Business/BrandBusiness/IBrandBusiness.cs
@@ -1,6 +1,8 @@
+using GoodOnYouScrapperAPI.Models;
+
 namespace GoodOnYouScrapperAPI.Business.BrandBusiness;
 
 public interface IBrandBusiness
 {
-    
+    public Task<ProcessingStatusResponse<BrandModel>> GetBrandInformation(string brandName);
 }

c957f2582d19b009dc426f3524943f33a6ec3017
Thomas Blondel <tfoenix@gmail.com>
2023-03-21 14:11:32 +0100

    [Controller] Brand controller created

---
+++ GoodOnYouScrapperAPI/Controllers/BrandController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodOnYouScrapperAPI.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+public class BrandController: ControllerBase
+{
+    
+}

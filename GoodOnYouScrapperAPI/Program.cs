b7b84f7a9f3052037828ecb86cb566b4d7985774
Thomas Blondel <tfoenix@gmail.com>
2023-03-22 12:09:25 +0100

    [Program.cs] Added versioning of the API

--- GoodOnYouScrapperAPI/Program.cs
+++ GoodOnYouScrapperAPI/Program.cs
@@ -1,7 +1,10 @@
 using GoodOnYouScrapperAPI.Business.BrandBusiness;
 using GoodOnYouScrapperAPI.DataAccess.BrandData;
+using GoodOnYouScrapperAPI.Utils.AppConstants;
 using GoodOnYouScrapperAPI.Utils.HttpClientWrapper;
 using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(opt =>
+{
+    opt.DefaultApiVersion = new ApiVersion(AppConstants.APIMajorVersion, AppConstants.APIMinorVersion);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("x-api-version"),
+        new MediaTypeApiVersionReader("x-api-version"));
+});
 
 // Dependency Injection
 builder.Services.AddHttpClient<IHttpClientWrapper, HttpClientWrapper>();

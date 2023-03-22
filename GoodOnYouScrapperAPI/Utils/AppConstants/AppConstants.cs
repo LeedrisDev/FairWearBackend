f81ea4dd6022b351c9ae335d7ebc00fddb09f24a
Thomas Blondel <tfoenix@gmail.com>
2023-03-22 12:09:04 +0100

    [AppConstants] Created to handle API major and minor versions

--- GoodOnYouScrapperAPI/Utils/AppConstants/AppConstants.cs
+++ GoodOnYouScrapperAPI/Utils/AppConstants/AppConstants.cs
@@ -3,4 +3,8 @@ namespace GoodOnYouScrapperAPI.Utils.AppConstants;
 public class AppConstants: IAppConstants
 {
     public static readonly string WebSiteUrl = "https://directory.goodonyou.eco/brand/";
+    
+    // API Versions
+    public static readonly int APIMajorVersion = 1;
+    public static readonly int APIMinorVersion = 0;
 }

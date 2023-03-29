2e55be7ecc3be14ecf3619dd0e9f9d0e0929a0d9
Thomas Blondel <tfoenix@gmail.com>
2023-03-29 18:33:06 +0200

    [Scrapper] Added the beginning of the controller

--- GoodOnYouScrapperAPI/Controllers/BrandController.cs
+++ GoodOnYouScrapperAPI/Controllers/BrandController.cs
@@ -1,10 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using GoodOnYouScrapperAPI.Business.BrandBusiness;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoodOnYouScrapperAPI.Controllers;
 
 [ApiController]
+[Route("[controller]")]
 [Produces("application/json")]
 public class BrandController: ControllerBase
 {
-    
+    private readonly IBrandBusiness _brandBusiness;
+
+    public BrandController(IBrandBusiness brandBusiness)
+    {
+        _brandBusiness = brandBusiness;
+    }
+
+    [HttpGet("{brandName}")]
+    public IActionResult GetBrand([Required] string brandName)
+    {
+        // TODO
+
+        return Ok();
+    }
 }

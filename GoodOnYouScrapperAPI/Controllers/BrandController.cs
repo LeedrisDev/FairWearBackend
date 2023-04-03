0010a6d36399112a6fb69838252974ea4bbd4cde
Thomas Blondel <tfoenix@gmail.com>
2023-04-03 14:42:56 +0200

    [Controller] Done creating BrandController

--- GoodOnYouScrapperAPI/Controllers/BrandController.cs
+++ GoodOnYouScrapperAPI/Controllers/BrandController.cs
@@ -1,11 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 using GoodOnYouScrapperAPI.Business.BrandBusiness;
+using GoodOnYouScrapperAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoodOnYouScrapperAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
 [Produces("application/json")]
 public class BrandController: ControllerBase
 {
@@ -17,10 +20,17 @@ public class BrandController: ControllerBase
     }
 
     [HttpGet("{brandName}")]
-    public IActionResult GetBrand([Required] string brandName)
+    [ProducesResponseType(typeof(BrandModel), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.NotFound)]
+    public async Task<IActionResult> GetBrand([Required] string brandName)
     {
-        // TODO
+        var brandInformation = await _brandBusiness.GetBrandInformation(brandName);
 
-        return Ok();
+        return brandInformation.Status switch
+        {
+            HttpStatusCode.OK => Ok(brandInformation.Object),
+            HttpStatusCode.NotFound => NotFound(brandInformation.ErrorMessage),
+            _ => StatusCode((int)brandInformation.Status, brandInformation.ErrorMessage)
+        };
     }
 }

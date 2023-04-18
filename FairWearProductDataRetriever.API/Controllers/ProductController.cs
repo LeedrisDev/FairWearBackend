80577251334616267a1c6d52ab524a2ef6b2e4ac
Thomas Blondel <tfoenix@gmail.com>
2023-04-18 10:39:03 +0200

    [Docs] Added for the controller

--- FairWearProductDataRetriever.API/Controllers/ProductController.cs
+++ FairWearProductDataRetriever.API/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FairWearProductDataRetriever.API.Controllers;
 
+/// <summary>Controller for retrieving product information.</summary>
 [ApiController]
 // [ApiVersion("1.0")]
 [Route("api/[controller]")]
@@ -14,16 +15,16 @@ public class ProductController: ControllerBase
 {
     private readonly IProductBusiness _productBusiness;
 
+    /// <summary>Constructor</summary>
+    /// <param name="productBusiness"> Product business.</param>
     public ProductController(IProductBusiness productBusiness)
     {
         _productBusiness = productBusiness;
     }
     
-    /// <summary>
-    /// Endpoint for retrieving product information.
-    /// </summary>
-    /// <param name="barcode"></param>
-    /// <returns></returns>
+    /// <summary> Get product information from a barcode.</summary>
+    /// <param name="barcode">Barcode of the product.</param>
+    /// <returns> Product information.</returns>
     [HttpGet("{barcode}")]
     [ProducesResponseType(typeof(ProductModel), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.NotFound)]

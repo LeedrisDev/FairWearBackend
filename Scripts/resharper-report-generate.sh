ac316403a483211d3d86863eec03924277ee112e
Thomas Blondel <tfoenix@gmail.com>
2023-04-20 02:53:13 +0200

    [Script] Updated parsing args

--- Scripts/resharper-report-generate.sh
+++ Scripts/resharper-report-generate.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
 # Parse script arguments
-while getopts ":p:" opt; do
-  case ${opt} in
-    p )
-      CSPROJ_PATH=$OPTARG
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -p|--project)
+      CSPROJ_PATH="$2"
+      shift
+      shift
       ;;
-    \? )
-      echo -e "\033[31mInvalid option: -$OPTARG\033[0m" 1>&2
-      echo "Usage: $0 -p <csproj_path>" >&2
-      exit 1
-      ;;
-    : )
-      echo -e "\033[31mInvalid option: -$OPTARG requires an argument\033[0m" 1>&2
-      echo "Usage: $0 -p <csproj_path>" >&2
+    *)    # unknown option
+      echo -e "\033[31mInvalid argument: $1\033[0m"
+      echo "Usage: $0 -p <csproj_path>"
       exit 1
       ;;
   esac
 done
-
-# Check if csproj path is provided
-if [[ -z "$csproj_path" ]]; then
-  echo -e "\033[31mError: You must provide a path to the csproj file using the -p option.\033[0m"
-  exit 1
-fi
 
 # Generate Resharper report
 jb inspectcode "$CSPROJ_PATH" \

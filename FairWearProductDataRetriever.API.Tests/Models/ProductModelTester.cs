854e6a59cc39447a45312e52376ee8cda33ef72b
Felix Ngo <felix.ngo@epita.fr>
2023-04-23 02:41:42 +0200

    [Model] Test product model upc code

--- FairWearProductDataRetriever.API.Tests/Models/ProductModelTester.cs
+++ FairWearProductDataRetriever.API.Tests/Models/ProductModelTester.cs
@@ -1,7 +1,20 @@
+using FairWearProductDataRetriever.API.Models;
+
 namespace ProductDataRetriever.Test.Models;
 
 [TestClass]
 public class ProductModelTester
 {
-    
+    [TestMethod]
+    public void TestUpcCode()
+    {
+        var product = new ProductModel
+        {
+            UpcCode = "193392069882"
+        };
+
+        Assert.AreEqual("193392069882", product.UpcCode);
+    }
+
+
 }

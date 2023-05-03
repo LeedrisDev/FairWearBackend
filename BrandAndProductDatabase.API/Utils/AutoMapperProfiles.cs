bbdda89c13d55c377386d2f24b76d2ba8c9fa8a2
Thomas Blondel <tfoenix@gmail.com>
2023-05-03 11:48:41 +0200

    [AutoMapper] Created file to handle mapping

---
+++ BrandAndProductDatabase.API/Utils/AutoMapperProfiles.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+
+namespace BrandAndProductDatabase.API.Utils;
+
+/// <summary>AutoMapper profiles, class representing the models to be mapped to each other.</summary>
+public class AutoMapperProfiles : Profile
+{
+    /// <summary>Constructor for AutoMapperProfiles</summary>
+    public AutoMapperProfiles()
+    {
+        
+    }
+}

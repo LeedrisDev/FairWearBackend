c7e14612708040bc0fd7dcbb5d4dd22eb04c7ab4
Thomas Blondel <tfoenix@gmail.com>
2023-05-03 13:00:47 +0200

    [IRepository] Updated return of functions and documentation too

--- BrandAndProductDatabase.API/DataAccess/IRepositories/IRepository.cs
+++ BrandAndProductDatabase.API/DataAccess/IRepositories/IRepository.cs
@@ -1,3 +1,5 @@
+using BrandAndProductDatabase.API.Models;
+
 namespace BrandAndProductDatabase.API.DataAccess.IRepositories;
 
 /// <summary>
@@ -5,29 +7,30 @@ namespace BrandAndProductDatabase.API.DataAccess.IRepositories;
 /// </summary>
 /// <typeparam name="TModel">The type of the entity for which the repository will be used.</typeparam>
 
-public interface IRepository<TModel>
+public interface IRepository<TModel> 
+    where TModel: class, IObjectWithId
 {
     /// <summary>Gets all entities from the data store.</summary>
-    /// <returns>An <see cref="IEnumerable{T}"/> containing all entities.</returns>
-    Task<IEnumerable<TModel>> GetAllAsync();
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task<ProcessingStatusResponse<IEnumerable<TModel>>> GetAllAsync();
 
     /// <summary>Gets a single entity from the data store by its ID.</summary>
     /// <param name="id">The ID of the entity to retrieve.</param>
-    /// <returns>The entity with the specified ID, or <see langword="null"/> if not found.</returns>
-    Task<TModel> GetByIdAsync(int id);
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task<ProcessingStatusResponse<TModel>> GetByIdAsync(int id);
 
     /// <summary>Adds a new entity to the data store.</summary>
     /// <param name="entity">The entity to add.</param>
     /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-    Task<TModel> AddAsync(TModel entity);
+    Task<ProcessingStatusResponse<TModel>> AddAsync(TModel entity);
 
     /// <summary>Updates an existing entity in the data store.</summary>
     /// <param name="entity">The entity to update.</param>
     /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-    Task<TModel> UpdateAsync(TModel entity);
+    Task<ProcessingStatusResponse<TModel>> UpdateAsync(TModel entity);
 
     /// <summary>Deletes an entity from the data store by its ID.</summary>
     /// <param name="id">The ID of the entity to delete.</param>
     /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-    Task<TModel> DeleteAsync(int id);
+    Task<ProcessingStatusResponse<TModel>> DeleteAsync(int id);
 }

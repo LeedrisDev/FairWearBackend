76dd45e20f114810a2130a79118747dfb3d71103
Thomas Blondel <tfoenix@gmail.com>
2023-05-03 11:17:37 +0200

    [DI] Registered DbContext

--- BrandAndProductDatabase.API/Program.cs
+++ BrandAndProductDatabase.API/Program.cs
@@ -1,11 +1,15 @@
+using BrandAndProductDatabase.API.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependency Injection
+builder.Services.AddDbContext<BrandAndProductDbContext>();
 
 var app = builder.Build();
 

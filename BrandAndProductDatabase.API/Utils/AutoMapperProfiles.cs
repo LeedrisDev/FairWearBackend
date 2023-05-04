b3894a6de424230f8505da70ecfeaa92ef43572f
felixngo <felix7051@gmail.com>
2023-05-04 10:05:51 +0200

    [Mapper] Mapping for Dto and Entity objects

--- BrandAndProductDatabase.API/Utils/AutoMapperProfiles.cs
+++ BrandAndProductDatabase.API/Utils/AutoMapperProfiles.cs
@@ -8,6 +8,10 @@ public class AutoMapperProfiles : Profile
     /// <summary>Constructor for AutoMapperProfiles</summary>
     public AutoMapperProfiles()
     {
-        
+        CreateMap<Models.Entity.BrandEntity, Models.Dto.BrandDto>();
+        CreateMap<Models.Dto.BrandDto, Models.Entity.BrandEntity>();
+
+        CreateMap<Models.Entity.ProductEntity, Models.Dto.ProductDto>();
+        CreateMap<Models.Dto.ProductDto, Models.Entity.ProductEntity>();
     }
 }

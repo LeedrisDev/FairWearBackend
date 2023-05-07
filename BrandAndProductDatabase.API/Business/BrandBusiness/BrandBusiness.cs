6ecf08c57d37d2d49670c590a982c0f549512709
felixngo <felix7051@gmail.com>
2023-05-07 13:37:19 +0200

    [BUSINESS] Create logic for BrandDb Actions

---
+++ BrandAndProductDatabase.API/Business/BrandBusiness/BrandBusiness.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using BrandAndProductDatabase.API.DataAccess.IRepositories;
+using BrandAndProductDatabase.API.Models.Dto;
+
+namespace BrandAndProductDatabase.API.Business.BrandBusiness;
+
+/// <summary>Business logic for brands database actions </summary>
+public class BrandBusiness : IBrandBusiness
+{
+    private readonly IBrandRepository _brandRepository;
+    private readonly IMapper _mapper;
+
+    /// <summary>
+    /// Constructor for BrandBusiness.
+    /// </summary>
+    /// <param name="brandRepository"></param>
+    /// <param name="mapper"></param>
+    public BrandBusiness(IBrandRepository brandRepository, IMapper mapper)
+    {
+        _brandRepository = brandRepository;
+        _mapper = mapper;
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<BrandDto>> GetAllBrandsAsync()
+    {
+        var brands = await _brandRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<BrandDto>>(brands);
+    }
+
+    /// <inheritdoc/>
+    public async Task<BrandDto> GetBrandByIdAsync(int id)
+    {
+        var brand = await _brandRepository.GetByIdAsync(id);
+        return _mapper.Map<BrandDto>(brand);
+    }
+
+    /// <inheritdoc/>
+    public async Task<BrandDto> CreateBrandAsync(BrandDto brandDto)
+    {
+        var brand = await _brandRepository.AddAsync(brandDto);
+        return _mapper.Map<BrandDto>(brand);
+    }
+
+    /// <inheritdoc/>
+    public async Task<BrandDto> UpdateBrandAsync(BrandDto brandDto)
+    {
+        var brand = await _brandRepository.GetByIdAsync(brandDto.Id);
+        if (brand == null)
+        {
+            // Handle the case where the brand doesn't exist
+            return null;
+        }
+
+        brand = await _brandRepository.UpdateAsync(brandDto);
+        return _mapper.Map<BrandDto>(brand);
+    }
+
+    /// <inheritdoc/>
+    public async Task DeleteBrandAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BrandAndProductDatabase.API/Business/BrandBusiness/IBrandBusiness.cs
@@ -0,0 +1,33 @@
+﻿using BrandAndProductDatabase.API.Models.Dto;
+
+namespace BrandAndProductDatabase.API.Business.BrandBusiness;
+
+/// <summary>
+/// Interface for BrandBusiness.
+/// </summary>
+public interface IBrandBusiness
+{
+    /// <summary>Gets all the Brands.</summary>
+    /// <returns>A list of <see cref="BrandDto"/>.</returns>
+    Task<IEnumerable<BrandDto>> GetAllBrandsAsync();
+
+    /// <summary>Gets a Brand by Id.</summary>
+    /// <param name="id">The Id of the Brand.</param>
+    /// <returns>A <see cref="BrandDto"/>.</returns>
+    Task<BrandDto> GetBrandByIdAsync(int id);
+
+    /// <summary>Creates a Brand.</summary>
+    /// <param name="brandDto">The Brand to create.</param>
+    /// <returns>A <see cref="BrandDto"/>.</returns>
+    Task<BrandDto> CreateBrandAsync(BrandDto brandDto);
+
+    /// <summary>Updates a Brand.</summary>
+    /// <param name="brandDto">The Brand to update.</param>
+    /// <returns>A <see cref="BrandDto"/>.</returns>
+    Task<BrandDto> UpdateBrandAsync(BrandDto brandDto);
+
+    /// <summary>Deletes a Brand.</summary>
+    /// <param name="id">The Id of the Brand to delete.</param>
+    /// <returns>A <see cref="Task"/>.</returns>
+    Task DeleteBrandAsync(int id);
+}

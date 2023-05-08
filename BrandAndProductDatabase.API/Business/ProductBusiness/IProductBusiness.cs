374a0992c4d6fa6d2e900a64df7fe2d7a0a88143
felixngo <felix7051@gmail.com>
2023-05-08 06:55:13 +0200

    [Business] Create business for products

---
+++ BrandAndProductDatabase.API/Business/ProductBusiness/IProductBusiness.cs
@@ -0,0 +1,30 @@
+﻿using BrandAndProductDatabase.API.Models;
+
+namespace BrandAndProductDatabase.API.Business.ProductBusiness;
+
+public interface IProductBusiness
+{
+    /// <summary>Gets all the Products.</summary>
+    /// <returns>A list of <see cref="Models.Dto.ProductDto"/>.</returns>
+    Task<ProcessingStatusResponse<IEnumerable<Models.Dto.ProductDto>>> GetAllProductsAsync();
+
+    /// <summary>Gets a Product by Id.</summary>
+    /// <param name="id">The Id of the Product.</param>
+    /// <returns>A <see cref="Models.Dto.ProductDto"/>.</returns>
+    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> GetProductByIdAsync(int id);
+
+    /// <summary>Creates a Product.</summary>
+    /// <param name="ProductDto">The Product to create.</param>
+    /// <returns>A <see cref="ProductDto"/>.</returns>
+    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> CreateProductAsync(Models.Dto.ProductDto ProductDto);
+
+    /// <summary>Updates a Product.</summary>
+    /// <param name="ProductDto">The Product to update.</param>
+    /// <returns>A <see cref="ProductDto"/>.</returns>
+    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> UpdateProductAsync(Models.Dto.ProductDto ProductDto);
+
+    /// <summary>Deletes a Product.</summary>
+    /// <param name="id">The Id of the Product to delete.</param>
+    /// <returns>A <see cref="Task"/>.</returns>
+    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> DeleteProductAsync(int id);
+}
---
+++ BrandAndProductDatabase.API/Business/ProductBusiness/ProductBusiness.cs
@@ -0,0 +1,49 @@
+﻿using BrandAndProductDatabase.API.DataAccess.IRepositories;
+using BrandAndProductDatabase.API.Models;
+using BrandAndProductDatabase.API.Models.Dto;
+
+namespace BrandAndProductDatabase.API.Business.ProductBusiness;
+
+public class ProductBusiness : IProductBusiness
+{
+    private readonly IProductRepository _productRepository;
+
+    /// <summary>
+    /// Constructor for ProductBusiness.
+    /// </summary>
+    /// <param name="productRepository"></param>
+    public ProductBusiness(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<IEnumerable<ProductDto>>> GetAllProductsAsync()
+    {
+        return await _productRepository.GetAllAsync();
+    }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<ProductDto>> GetProductByIdAsync(int id)
+    {
+        return await _productRepository.GetByIdAsync(id);
+    }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<ProductDto>> CreateProductAsync(ProductDto productDto)
+    {
+        return await _productRepository.AddAsync(productDto);
+    }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<ProductDto>> UpdateProductAsync(ProductDto productDto)
+    {
+        return await _productRepository.UpdateAsync(productDto);
+    }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<ProductDto>> DeleteProductAsync(int id)
+    {
+        return await _productRepository.DeleteAsync(id);
+    }
+}

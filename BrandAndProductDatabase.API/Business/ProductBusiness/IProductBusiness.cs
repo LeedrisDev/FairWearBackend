d297dc884637ef9a78362ec24b9eb7d86f0925c8
felixngo <felix7051@gmail.com>
2023-05-08 08:04:01 +0200

    [Linter] Code smells again

--- BrandAndProductDatabase.API/Business/ProductBusiness/IProductBusiness.cs
+++ BrandAndProductDatabase.API/Business/ProductBusiness/IProductBusiness.cs
@@ -10,25 +10,25 @@ public interface IProductBusiness
 {
     /// <summary>Gets all the Products.</summary>
     /// <returns>A list of <see cref="Models.Dto.ProductDto"/>.</returns>
-    Task<ProcessingStatusResponse<IEnumerable<Models.Dto.ProductDto>>> GetAllProductsAsync();
+    Task<ProcessingStatusResponse<IEnumerable<ProductDto>>> GetAllProductsAsync();
 
     /// <summary>Gets a Product by Id.</summary>
     /// <param name="id">The Id of the Product.</param>
     /// <returns>A <see cref="Models.Dto.ProductDto"/>.</returns>
-    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> GetProductByIdAsync(int id);
+    Task<ProcessingStatusResponse<ProductDto>> GetProductByIdAsync(int id);
 
     /// <summary>Creates a Product.</summary>
     /// <param name="productDto">The Product to create.</param>
     /// <returns>A <see cref="productDto"/>.</returns>
-    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> CreateProductAsync(Models.Dto.ProductDto productDto);
+    Task<ProcessingStatusResponse<ProductDto>> CreateProductAsync(ProductDto productDto);
 
     /// <summary>Updates a Product.</summary>
     /// <param name="productDto">The Product to update.</param>
     /// <returns>A <see cref="ProductDto"/>.</returns>
-    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> UpdateProductAsync(Models.Dto.ProductDto productDto);
+    Task<ProcessingStatusResponse<ProductDto>> UpdateProductAsync(ProductDto productDto);
 
     /// <summary>Deletes a Product.</summary>
     /// <param name="id">The Id of the Product to delete.</param>
     /// <returns>A <see cref="Task"/>.</returns>
-    Task<ProcessingStatusResponse<Models.Dto.ProductDto>> DeleteProductAsync(int id);
+    Task<ProcessingStatusResponse<ProductDto>> DeleteProductAsync(int id);
 }

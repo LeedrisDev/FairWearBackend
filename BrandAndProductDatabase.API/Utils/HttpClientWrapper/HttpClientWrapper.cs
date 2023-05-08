7455a2f69f964f49562837fc4779fa19bb659196
felixngo <felix7051@gmail.com>
2023-05-08 04:16:26 +0200

    [Controller] Add HtppCLient Wrappers

---
+++ BrandAndProductDatabase.API/Utils/HttpClientWrapper/HttpClientWrapper.cs
@@ -0,0 +1,20 @@
+namespace BrandAndProductDatabase.API.Utils.HttpClientWrapper;
+
+/// <summary>Wrapper for the <see cref="HttpClient"/> class</summary>
+public class HttpClientWrapper : IHttpClientWrapper
+{
+    private readonly HttpClient _httpClient;
+
+    /// <summary>Constructor</summary>
+    /// <param name="httpClient"> <see cref="HttpClient"/> to use for the requests</param>
+    public HttpClientWrapper(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    /// <inheritdoc />
+    public async Task<HttpResponseMessage> GetAsync(string requestUri)
+    {
+        return await _httpClient.GetAsync(requestUri);
+    }
+}

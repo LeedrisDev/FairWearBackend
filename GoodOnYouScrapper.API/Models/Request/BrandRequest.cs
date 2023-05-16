aeb2e081fa1b24cbb48002778581328e5efb2f23
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 17:38:45 +0200

    [Models] Created BrandRequest

---
+++ GoodOnYouScrapper.API/Models/Request/BrandRequest.cs
@@ -0,0 +1,8 @@
+namespace GoodOnYouScrapper.API.Models.Request;
+
+/// <summary>Class representing a brand for a request.</summary>
+public class BrandRequest
+{
+    /// <summary>Name of the brand.</summary>
+    public string Name { get; set; } = string.Empty;
+}

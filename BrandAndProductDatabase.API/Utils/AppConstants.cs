6f8ffb124a5ac8a89247acebce850d799db8fc28
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 18:44:50 +0200

    [BrandAndProductAPI] Created AppConstants

---
+++ BrandAndProductDatabase.API/Utils/AppConstants.cs
@@ -0,0 +1,8 @@
+namespace BrandAndProductDatabase.API.Utils;
+
+/// <summary>Class to hold constants for the application.</summary>
+public static class AppConstants
+{
+    /// <summary>URL for the Good On You Scrapper microservice.</summary>
+    public static string GOOD_ON_YOU_SCRAPPER_URL = "http://good_on_you_scrapper_api:80/api/brand";
+}

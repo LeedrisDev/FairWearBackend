9e463ba4a95aa758f0a979709b3801f9675ae901
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 18:08:59 +0200

    [BrandRepository] Added method to get a brand by its name

--- BrandAndProductDatabase.API/DataAccess/IRepositories/IBrandRepository.cs
+++ BrandAndProductDatabase.API/DataAccess/IRepositories/IBrandRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace BrandAndProductDatabase.API.DataAccess.IRepositories;
+﻿using BrandAndProductDatabase.API.Models;
+using BrandAndProductDatabase.API.Models.Dto;
+
+namespace BrandAndProductDatabase.API.DataAccess.IRepositories;
 
 /// <summary>Interface for BrandRepository</summary>
 public interface IBrandRepository : IRepository<Models.Dto.BrandDto>
 {
-
+    /// <summary>Gets a single brand by its name.</summary>
+    /// <param name="name">The name of the brand to get.</param>
+    /// <returns>A single brand.</returns>
+    Task<ProcessingStatusResponse<BrandDto>> GetBrandByNameAsync(string name);
 }
--- BrandAndProductDatabase.API/DataAccess/Repositories/BrandRepository.cs
+++ BrandAndProductDatabase.API/DataAccess/Repositories/BrandRepository.cs
@@ -1,12 +1,35 @@
-﻿using AutoMapper;
+﻿using System.Net;
+using AutoMapper;
 using BrandAndProductDatabase.API.DataAccess.IRepositories;
+using BrandAndProductDatabase.API.Models;
+using BrandAndProductDatabase.API.Models.Dto;
+using BrandAndProductDatabase.API.Models.Entity;
 using Microsoft.EntityFrameworkCore;
 
 namespace BrandAndProductDatabase.API.DataAccess.Repositories;
 
 /// <summary>Class representing a repository for the BrandEntity.</summary>
-public class BrandRepository : Repository<Models.Dto.BrandDto, Models.Entity.BrandEntity>, IBrandRepository
+public class BrandRepository : Repository<BrandDto, BrandEntity>, IBrandRepository
 {
     /// <summary>Constructor for BrandRepository</summary>
     public BrandRepository(DbContext context, IMapper mapper) : base(context, mapper) { }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<BrandDto>> GetBrandByNameAsync(string name)
+    {
+        var processingStatusResponse = new ProcessingStatusResponse<BrandDto>();
+        
+        var brand = await DbSet.FirstOrDefaultAsync(x => x.Name == name);
+        if (brand == null)
+        {
+            return new ProcessingStatusResponse<BrandDto>
+            {
+                Status = HttpStatusCode.NotFound,
+                ErrorMessage = $"Brand with name {name} not found."
+            };
+        }
+        
+        processingStatusResponse.Object = Mapper.Map<BrandDto>(brand);
+        return processingStatusResponse;
+    }
 }

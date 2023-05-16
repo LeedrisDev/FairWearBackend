0589361864f853a07f6aec44672ec18613ed4d01
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 17:03:18 +0200

    [Doc] Updated on interface for BrandData

--- FairWearGateway.API/DataAccess/BrandData/IBrandData.cs
+++ FairWearGateway.API/DataAccess/BrandData/IBrandData.cs
@@ -7,13 +7,13 @@ namespace FairWearGateway.API.DataAccess.BrandData;
 /// <summary>Interface that defines the methods to call the appropriate microservice to get all is related to brands.</summary>
 public interface IBrandData
 {
-    /// <summary>Gets all the brands in the database</summary>
+    /// <summary>Call the appropriate microservice to get all the brands in the database.</summary>
     /// <returns>
     /// A <see cref="ProcessingStatusResponse{T}"/> that contains the list of brand which are in database
     /// </returns>
     Task<ProcessingStatusResponse<IEnumerable<BrandResponse>>> GetAllBrandsAsync();
 
-    /// <summary>Get a brand from it's Id</summary>
+    /// <summary>Call the appropriate microservice to get a brand by its id.</summary>
     /// <param name="brandId">Id of the wanted brand</param>
     /// <returns>
     /// A <see cref="ProcessingStatusResponse{T}"/> instance, that contains the brand object if the call succeed

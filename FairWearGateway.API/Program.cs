e6b607ad778fd72f8fb52e85d4b8b106367c637f
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 16:42:36 +0200

    [DI] Externalized configuration of dependency injection

--- FairWearGateway.API/Program.cs
+++ FairWearGateway.API/Program.cs
@@ -1,9 +1,4 @@
-using FairWearGateway.API.Business.BrandBusiness;
-using FairWearGateway.API.Business.ProductBusiness;
 using FairWearGateway.API.Config;
-using FairWearGateway.API.DataAccess.BrandData;
-using FairWearGateway.API.DataAccess.ProductData;
-using FairWearGateway.API.Utils.HttpClientWrapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Dependency Injection
-builder.Services.AddHttpClient<IHttpClientWrapper, HttpClientWrapper>();
-
-builder.Services.AddTransient<IProductData, ProductData>();
-builder.Services.AddTransient<IBrandData, BrandData>();
-
-builder.Services.AddTransient<IProductBusiness, ProductBusiness>();
-builder.Services.AddTransient<IBrandBusiness, BrandBusiness>();
+DependencyInjectionConfiguration.Configure(builder.Services);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

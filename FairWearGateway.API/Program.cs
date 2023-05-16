f25729ebd2ecf7acb864788c2b3efe9665c7a1ed
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 15:17:25 +0200

    [Gateway] Swagger is accessible even environment is not production

--- FairWearGateway.API/Program.cs
+++ FairWearGateway.API/Program.cs
@@ -7,7 +7,6 @@ using FairWearGateway.API.Utils.HttpClientWrapper;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Dependency Injection
@@ -26,11 +25,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
 

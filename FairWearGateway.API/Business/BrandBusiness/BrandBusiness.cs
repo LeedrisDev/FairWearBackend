84be7cd46ae0862c6e71615ff6809640b99691cb
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 17:03:49 +0200

    [Business] Added first function for Brand

--- FairWearGateway.API/Business/BrandBusiness/BrandBusiness.cs
+++ FairWearGateway.API/Business/BrandBusiness/BrandBusiness.cs
@@ -1,8 +1,30 @@
+using FairWearGateway.API.DataAccess.BrandData;
+using FairWearGateway.API.Models;
+using FairWearGateway.API.Models.Response;
+
 namespace FairWearGateway.API.Business.BrandBusiness;
 
 
 /// <summary>Class that handles the business logic for the Brand model.</summary>
 public class BrandBusiness : IBrandBusiness
 {
+    private readonly IBrandData _brandData;
+
+    /// <summary>Initializes a new instance of the <see cref="BrandBusiness"/> class.</summary>
+    public BrandBusiness(IBrandData brandData)
+    {
+        _brandData = brandData;
+    }
     
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<IEnumerable<BrandResponse>>> GetAllBrandsAsync()
+    {
+        return await _brandData.GetAllBrandsAsync();
+    }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<BrandResponse>> GetBrandByIdAsync(int brandId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- FairWearGateway.API/Business/BrandBusiness/IBrandBusiness.cs
+++ FairWearGateway.API/Business/BrandBusiness/IBrandBusiness.cs
@@ -1,7 +1,21 @@
+using FairWearGateway.API.Models;
+using FairWearGateway.API.Models.Response;
+
 namespace FairWearGateway.API.Business.BrandBusiness;
 
 /// <summary>Interface that handles the business logic for the Brand model.</summary>
 public interface IBrandBusiness
 {
+    /// <summary>Call the data access to get all the brands in the database.</summary>
+    /// <returns>
+    /// A <see cref="ProcessingStatusResponse{T}"/> that contains the list of brand which are in database
+    /// </returns>
+    Task<ProcessingStatusResponse<IEnumerable<BrandResponse>>> GetAllBrandsAsync();
     
+    /// <summary>Call the data access to get a brand by its id.</summary>
+    /// <param name="brandId">Id of the wanted brand</param>
+    /// <returns>
+    /// A <see cref="ProcessingStatusResponse{T}"/> instance, that contains the brand object if the call succeed
+    /// </returns>
+    Task<ProcessingStatusResponse<BrandResponse>> GetBrandByIdAsync(int brandId);
 }

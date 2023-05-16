381739ded001cd1ee3d2209b0311e71fd9b23476
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 15:39:50 +0200

    [Gateway] Updated URL to acces brand & product service

--- FairWearGateway.API/Utils/AppConstants.cs
+++ FairWearGateway.API/Utils/AppConstants.cs
@@ -2,5 +2,5 @@ namespace FairWearGateway.API.Utils;
 
 public static class AppConstants
 {
-    public const string BRAND_AND_PRODUCT_API_URL = "http://localhost:8081/api/brands";
+    public const string BRAND_AND_PRODUCT_API_URL = "http://brand_and_product_api:80/api/brands";
 }

d78e8a4e541f89d9b73fce9376ea7f954ed3f8ac
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 15:15:21 +0200

    [Controller] Created first version of controller for brands in Gateway

--- FairWearGateway.API/Controllers/BrandsController.cs
+++ FairWearGateway.API/Controllers/BrandsController.cs
@@ -1,10 +1,26 @@
+using System.Net;
+using FairWearGateway.API.DataAccess.BrandData;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FairWearGateway.API.Controllers;
 
 [ApiController]
 [Route("/api/[controller]")]
+[Produces("application/json")]
 public class BrandsController : ControllerBase
 {
+    private readonly IBrandData _brandData;
+
+    public BrandsController(IBrandData brandData)
+    {
+        _brandData = brandData;
+    }
     
+    [HttpGet]
+    public async Task<IActionResult> GetAllBrandsAsync()
+    {
+        var processingStatusResponse = await _brandData.GetAllBrandsAsync();
+        
+        return processingStatusResponse.Status != HttpStatusCode.OK ? StatusCode((int) processingStatusResponse.Status, processingStatusResponse.ErrorMessage) : Ok(processingStatusResponse.Object);
+    }
 }

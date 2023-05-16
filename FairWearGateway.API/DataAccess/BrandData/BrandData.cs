1b304baa5d59079153d39ff75b5d78717ef8de0f
Thomas Blondel <tfoenix@gmail.com>
2023-05-16 14:28:07 +0200

    [Data] Added logic to access brand(s) in gateway

--- FairWearGateway.API/DataAccess/BrandData/BrandData.cs
+++ FairWearGateway.API/DataAccess/BrandData/BrandData.cs
@@ -1,6 +1,82 @@
+using System.Text.Json;
+using FairWearGateway.API.Models;
+using FairWearGateway.API.Models.ServiceResponse;
+using FairWearGateway.API.Utils;
+using FairWearGateway.API.Utils.HttpClientWrapper;
+
 namespace FairWearGateway.API.DataAccess.BrandData;
 
 public class BrandData : IBrandData
 {
+    private readonly IHttpClientWrapper _httpClientWrapper;
+
+    public BrandData(IHttpClientWrapper httpClientWrapper)
+    {
+        _httpClientWrapper = httpClientWrapper;
+    }
     
+    public async Task<ProcessingStatusResponse<IEnumerable<BrandServiceResponse>>> GetAllBrandsAsync()
+    {
+        var processingStatusResponse = new ProcessingStatusResponse<IEnumerable<BrandServiceResponse>>();
+        
+        var response = await _httpClientWrapper.GetAsync(AppConstants.BRAND_AND_PRODUCT_API_URL);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            processingStatusResponse.Status = response.StatusCode;
+            processingStatusResponse.ErrorMessage = response.ReasonPhrase ?? "Error while getting brands";
+            return processingStatusResponse;
+        }
+
+        try
+        {
+            var brands = await DeserializeResponse<IEnumerable<BrandServiceResponse>>(response);
+            processingStatusResponse.Object = brands;
+            return processingStatusResponse;
+        }
+        catch (ApplicationException e)
+        {
+            processingStatusResponse.Status = System.Net.HttpStatusCode.InternalServerError;
+            processingStatusResponse.ErrorMessage = e.Message;
+            return processingStatusResponse;
+        }
+    }
+
+    public async Task<ProcessingStatusResponse<BrandServiceResponse>> GetBrandByIdAsync(int brandId)
+    {
+        var processingStatusResponse = new ProcessingStatusResponse<BrandServiceResponse>();
+        
+        var response = await _httpClientWrapper.GetAsync($"{AppConstants.BRAND_AND_PRODUCT_API_URL}/{brandId}");
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            processingStatusResponse.Status = response.StatusCode;
+            processingStatusResponse.ErrorMessage = response.ReasonPhrase ?? "Error while getting brand";
+            return processingStatusResponse;
+        }
+        
+        try
+        {
+            var brand = await DeserializeResponse<BrandServiceResponse>(response);
+            processingStatusResponse.Object = brand;
+            return processingStatusResponse;
+        }
+        catch (ApplicationException e)
+        {
+            processingStatusResponse.Status = System.Net.HttpStatusCode.InternalServerError;
+            processingStatusResponse.ErrorMessage = e.Message;
+            return processingStatusResponse;
+        }
+    }
+    
+    private static async Task<T> DeserializeResponse<T>(HttpResponseMessage response)
+    {
+        var responseString = await response.Content.ReadAsStringAsync();
+        var deserializedObject = JsonSerializer.Deserialize<T>(responseString);
+
+        if (deserializedObject == null)
+            throw new ApplicationException("Cannot deserialize response");
+
+        return deserializedObject;
+    }
 }
--- FairWearGateway.API/DataAccess/BrandData/IBrandData.cs
+++ FairWearGateway.API/DataAccess/BrandData/IBrandData.cs
@@ -1,6 +1,20 @@
+using FairWearGateway.API.Models;
+using FairWearGateway.API.Models.ServiceResponse;
+
 namespace FairWearGateway.API.DataAccess.BrandData;
 
 public interface IBrandData
 {
-    
+    /// <summary>Gets all the brands in the database</summary>
+    /// <returns>
+    /// A <see cref="ProcessingStatusResponse{T}"/> that contains the list of brand which are in database
+    /// </returns>
+    Task<ProcessingStatusResponse<IEnumerable<BrandServiceResponse>>> GetAllBrandsAsync();
+
+    /// <summary>Get a brand from it's Id</summary>
+    /// <param name="brandId">Id of the wanted brand</param>
+    /// <returns>
+    /// A <see cref="ProcessingStatusResponse{T}"/> instance, that contains the brand object if the call succeed
+    /// </returns>
+    Task<ProcessingStatusResponse<BrandServiceResponse>> GetBrandByIdAsync(int brandId);
 }

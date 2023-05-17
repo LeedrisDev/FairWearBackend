8110814c9d1e46dc4291c1ab4972aaec97b37d53
Thomas Blondel <tfoenix@gmail.com>
2023-05-17 22:00:22 +0200

    [Gateway Models] Created model to handle requests related to brands

---
+++ FairWearGateway.API/Models/Request/BrandRequest.cs
@@ -0,0 +1,8 @@
+namespace FairWearGateway.API.Models.Request;
+
+/// <summary>Class that represents a brand request.</summary>
+public class BrandRequest
+{
+    /// <summary>Name of the brand.</summary>
+    public string Name { get; set; } = string.Empty;
+}

59f0d388168ff61d61b979a530cdd7d984abe7e9
Thomas Blondel <tfoenix@gmail.com>
2023-05-18 03:15:54 +0200

    [PDR Swagger] Put swagger at /api/swagger

--- FairWearProductDataRetriever.API/Program.cs
+++ FairWearProductDataRetriever.API/Program.cs
@@ -1,10 +1,8 @@
-using System.Reflection;
 using HtmlAgilityPack;
 using FairWearProductDataRetriever.API.Business.ProductBusiness;
+using FairWearProductDataRetriever.API.Config;
 using FairWearProductDataRetriever.API.DataAccess.ProductData;
-using FairWearProductDataRetriever.API.Utils;
 using FairWearProductDataRetriever.API.Utils.HttpClientWrapper;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,23 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = AppConstants.ApiName,
-        Version = "v1",
-        Description = AppConstants.ApiDescription,
-        Contact = new OpenApiContact
-        {
-            Name = "FairWear Group",
-            Email = AppConstants.FairWearMail
-        }
-    });
-    
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+
+SwaggerConfiguration.Configure(builder.Services);
 
 // Dependency Injection
 builder.Services.AddHttpClient<IHttpClientWrapper, HttpClientWrapper>();
@@ -39,15 +22,13 @@ builder.Services.AddTransient<IProductBusiness, ProductBusiness>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = "api/swagger";
+});
 
 app.UseHttpsRedirection();
 

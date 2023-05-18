78f4e72306f8a5b48a6f84bdc78ea06521824e32
Thomas Blondel <tfoenix@gmail.com>
2023-05-18 17:35:36 +0200

    [B&P AppConstants] Added constant

--- BrandAndProductDatabase.API/Utils/AppConstants.cs
+++ BrandAndProductDatabase.API/Utils/AppConstants.cs
@@ -5,4 +5,7 @@ public static class AppConstants
 {
     /// <summary>URL for the Good On You Scrapper microservice.</summary>
     public const string GoodOnYouScrapperUrl = "http://good_on_you_scrapper_api:80/api/brand";
+
+    /// <summary>URL for the Product Data Retriever microservice.</summary>
+    public const string ProductDataRetrieverUrl = "http://product_data_retriever_api:80/api/product";
 }

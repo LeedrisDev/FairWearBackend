9a51d8c7e2638896f317b6dd6b03f06d00cfec3d
Thomas Blondel <tfoenix@gmail.com>
2023-05-18 17:37:16 +0200

    [B&P Data] Created Product data to contact data retriever

---
+++ BrandAndProductDatabase.API/DataAccess/ProductData/IProductData.cs
@@ -0,0 +1,15 @@
+using BrandAndProductDatabase.API.Models;
+using BrandAndProductDatabase.API.Models.Dto;
+
+namespace BrandAndProductDatabase.API.DataAccess.ProductData;
+
+/// <summary>Interface that defines the methods for ProductData.</summary>
+public interface IProductData
+{
+    /// <summary>
+    /// Call the appropriate microservice to get a product by its name.
+    /// </summary>
+    /// <param name="name">Name of the product to get.</param>
+    /// <returns> A <see cref="ProcessingStatusResponse{T}"/> containing the product. If the product exists.</returns>
+    Task<ProcessingStatusResponse<ProductDto>> GetProductByBarcode(string barcode);
+}

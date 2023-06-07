5348c48e80761822714f89c713a5fee9dd21f9a3
felixngo <felix@felix>
2023-06-07 18:26:46 +0200

    [Refact] Remove get product by barcode

--- BrandAndProductDatabase.API/DataAccess/ProductData/IProductData.cs
+++ BrandAndProductDatabase.API/DataAccess/ProductData/IProductData.cs
@@ -6,10 +6,4 @@ namespace BrandAndProductDatabase.API.DataAccess.ProductData;
 /// <summary>Interface that defines the methods for ProductData.</summary>
 public interface IProductData
 {
-    /// <summary>
-    /// Call the appropriate microservice to get a product by its name.
-    /// </summary>
-    /// <param name="barcode">Name of the product to get.</param>
-    /// <returns> A <see cref="ProcessingStatusResponse{T}"/> containing the product. If the product exists.</returns>
-    Task<ProcessingStatusResponse<ProductDto>> GetProductByBarcode(string barcode);
 }

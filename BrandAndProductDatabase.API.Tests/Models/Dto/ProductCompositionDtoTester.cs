98964bd075969e64335038e627b94979c2759df3
felixngo <felix@felix>
2023-06-07 11:25:24 +0200

    [DTO] Add product composition Dto tests

---
+++ BrandAndProductDatabase.API.Tests/Models/Dto/ProductCompositionDtoTester.cs
@@ -0,0 +1,41 @@
+﻿using BrandAndProductDatabase.API.Models.Dto;
+using FluentAssertions;
+
+namespace BrandAndProductDatabase.API.Tests.Models.Dto;
+
+public class ProductCompositionDtoTester
+{
+    [TestClass]
+    public class ProductCompositionDtoTests
+    {
+        [TestMethod]
+        public void ProductCompositionDto_SetAndGetPercentage_ShouldSetAndReturnPercentage()
+        {
+            // Arrange
+            var productCompositionDto = new ProductCompositionDto();
+            var percentage = 75;
+
+            // Act
+            productCompositionDto.Percentage = percentage;
+            var result = productCompositionDto.Percentage;
+
+            // Assert
+            result.Should().Be(percentage);
+        }
+
+        [TestMethod]
+        public void ProductCompositionDto_SetAndGetComponent_ShouldSetAndReturnComponent()
+        {
+            // Arrange
+            var productCompositionDto = new ProductCompositionDto();
+            var component = "Cotton";
+
+            // Act
+            productCompositionDto.Component = component;
+            var result = productCompositionDto.Component;
+
+            // Assert
+            result.Should().Be(component);
+        }
+    }   
+}

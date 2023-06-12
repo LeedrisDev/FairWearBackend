db06583c81830580da70b3476f826a6ac980f59e
felixngo <felix.ngo@epita.fr>
2023-06-12 12:36:08 +0200

    [GATEWAY] Fix interface

--- FairWearGateway.API/DataAccess/ProductData/ProductData.cs
+++ FairWearGateway.API/DataAccess/ProductData/ProductData.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using BrandAndProductDatabase.API.Models.Response;
 using FairWearGateway.API.Models;
 using FairWearGateway.API.Utils;
@@ -17,49 +16,21 @@ public class ProductData : IProductData
     {
         _httpClientWrapper = httpClientWrapper;
     }
-    
-    /// <inheritdoc />
-    public async Task<ProcessingStatusResponse<IEnumerable<ProductResponse>>> GetAllProductsAsync()
-    {
-        var processingStatusResponse = new ProcessingStatusResponse<IEnumerable<ProductResponse>>();
-        
-        var response = await _httpClientWrapper.GetAsync($"{AppConstants.BrandAndProductApiUrl}/products");
-
-        if (!response.IsSuccessStatusCode)
-        {
-            processingStatusResponse.Status = response.StatusCode;
-            processingStatusResponse.ErrorMessage = response.ReasonPhrase ?? "Error while getting products";
-            return processingStatusResponse;
-        }
-
-        try
-        {
-            var products = await DeserializeResponse<IEnumerable<ProductResponse>>(response);
-            processingStatusResponse.Object = products;
-            return processingStatusResponse;
-        }
-        catch (ApplicationException e)
-        {
-            processingStatusResponse.Status = System.Net.HttpStatusCode.InternalServerError;
-            processingStatusResponse.ErrorMessage = e.Message;
-            return processingStatusResponse;
-        }
-    }
 
     /// <inheritdoc />
     public async Task<ProcessingStatusResponse<ProductResponse>> GetProductByIdAsync(int productId)
     {
         var processingStatusResponse = new ProcessingStatusResponse<ProductResponse>();
-        
+
         var response = await _httpClientWrapper.GetAsync($"{AppConstants.BrandAndProductApiUrl}/product/{productId}");
-        
+
         if (!response.IsSuccessStatusCode)
         {
             processingStatusResponse.Status = response.StatusCode;
             processingStatusResponse.ErrorMessage = response.ReasonPhrase ?? "Error while getting product";
             return processingStatusResponse;
         }
-        
+
         try
         {
             var product = await DeserializeResponse<ProductResponse>(response);
@@ -73,7 +44,7 @@ public class ProductData : IProductData
             return processingStatusResponse;
         }
     }
-    
+
 
     private static async Task<T> DeserializeResponse<T>(HttpResponseMessage response)
     {

4928b43a8ef56a629de237691d300b0bb347fa15
felixngo <felix.ngo@epita.fr>
2023-06-16 13:35:11 +0200

    [Models] Test ErrorResponse

---
+++ FairWearGateway.API.Tests/Models/Response/ErrorResponseTester.cs
@@ -0,0 +1,21 @@
+﻿using FairWearGateway.API.Models.Response;
+using FluentAssertions;
+
+namespace FairWearGateway.API.Tests.Models.Response;
+
+[TestClass]
+public class ErrorResponseTester
+{
+    [TestMethod]
+    public void BrandResponse_Ranges_ShouldBeSettable()
+    {
+        // Arrange
+        var errorResponse = new ErrorResponse();
+
+        // Act
+        errorResponse.Message = "Error";
+
+        // Assert
+        errorResponse.Message.Should().BeEquivalentTo("Error");
+    }
+}

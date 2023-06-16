2ac2e34fd960c6a0b25a201920c600d1f94bfab9
felixngo <felix.ngo@epita.fr>
2023-06-16 13:39:37 +0200

    [Models] Test product score response

---
+++ FairWearGateway.API.Tests/Models/Response/ProductScoreResponseTester.cs
@@ -0,0 +1,47 @@
+﻿using FairWearGateway.API.Models.Response;
+using FluentAssertions;
+
+namespace FairWearGateway.API.Tests.Models.Response;
+
+[TestClass]
+public class ProductScoreResponseTester
+{
+    [TestMethod]
+    public void ProductScoreResponse_Moral_ShouldBeSettable()
+    {
+        // Arrange
+        var productScoreResponse = new ProductScoreResponse();
+
+        // Act
+        productScoreResponse.Moral = 8;
+
+        // Assert
+        productScoreResponse.Moral.Should().Be(8);
+    }
+
+    [TestMethod]
+    public void ProductScoreResponse_Animal_ShouldBeSettable()
+    {
+        // Arrange
+        var productScoreResponse = new ProductScoreResponse();
+
+        // Act
+        productScoreResponse.Animal = 6;
+
+        // Assert
+        productScoreResponse.Animal.Should().Be(6);
+    }
+
+    [TestMethod]
+    public void ProductScoreResponse_Environmental_ShouldBeSettable()
+    {
+        // Arrange
+        var productScoreResponse = new ProductScoreResponse();
+
+        // Act
+        productScoreResponse.Environmental = 7;
+
+        // Assert
+        productScoreResponse.Environmental.Should().Be(7);
+    }
+}

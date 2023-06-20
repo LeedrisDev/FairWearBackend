cc8585ac408be41eb7409ff6addd427dd7072169
felixngo <felix.ngo@epita.fr>
2023-06-20 09:57:07 +0200

    [Test] Test the http client wrapper

---
+++ FairWearGateway.API.Tests/Utils/HttpClientWrapper/HttpClientWrapperTester.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using FluentAssertions;
+using Moq;
+
+namespace FairWearGateway.API.Tests.Utils.HttpClientWrapper;
+
+[TestClass]
+public class HttpClientWrapperTester
+{
+    private readonly Mock<HttpClient> _httpClientMock = new();
+
+    [TestMethod]
+    public async Task GetAsync_WithGoogleUrl_ThenStatusCodeIsOK()
+    {
+        // Arrange
+        const HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
+        var httpClientWrapper = new API.Utils.HttpClientWrapper.HttpClientWrapper(_httpClientMock.Object);
+        const string requestUri = "https://www.google.com";
+
+        // Act
+        var result = await httpClientWrapper.GetAsync(requestUri);
+
+        // Assert
+        result.StatusCode.Should().Be(expectedStatusCode);
+    }
+
+    [TestMethod]
+    public async Task GetAsync_WithGoogleUrl_ThenContentIsNotNull()
+    {
+        // Arrange
+        var httpClientWrapper = new API.Utils.HttpClientWrapper.HttpClientWrapper(_httpClientMock.Object);
+        const string requestUri = "https://www.google.com";
+
+        // Act
+        var result = await httpClientWrapper.GetAsync(requestUri);
+
+        // Assert
+        result.Content.Should().NotBeNull();
+    }
+}

efd3d3e83b35a6d82d5b07d7cde518f6d59b860f
felixngo <felix.ngo@epita.fr>
2023-07-06 16:55:20 +0200

    [SCRAPPER] Add grpc service for scrapper good on you

---
+++ GoodOnYouScrapper.Service/Services/BrandService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using System.Net;
+using GoodOnYouScrapper.Service.Business.BrandBusiness;
+using GoodOnYouScrapper.Service.Proto;
+
+namespace GoodOnYouScrapper.Service.Services
+{
+    public class BrandService : Proto.BrandService.BrandServiceBase
+    {
+        private readonly ILogger<BrandService> _logger;
+        private readonly IBrandBusiness _brandBusiness;
+
+        public BrandService(IBrandBusiness brandBusiness, ILogger<BrandService> logger)
+        {
+            _brandBusiness = brandBusiness;
+            _logger = logger;
+        }
+
+        public override async Task<BrandResponse> GetBrand(BrandRequest request, ServerCallContext context)
+        {
+            var brandInformation = await _brandBusiness.GetBrandInformation(request.Name);
+
+            return brandInformation.Status switch
+            {
+                HttpStatusCode.NotFound => throw new RpcException(new Status(StatusCode.NotFound, brandInformation.ErrorMessage)),
+                HttpStatusCode.OK => brandInformation.Object,
+                _ => throw new RpcException(new Status(StatusCode.Internal, brandInformation.ErrorMessage))
+            };
+        }
+        
+    }
+}

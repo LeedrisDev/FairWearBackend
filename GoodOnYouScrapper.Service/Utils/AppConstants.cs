91235d8a5574fe269603338c53b4f1019085807d
Thomas Blondel <tfoenix@gmail.com>
2023-07-08 21:13:02 +0200

    [Refacto] Variables in AppConstants in GOYScrapper switched to const

--- GoodOnYouScrapper.Service/Utils/AppConstants.cs
+++ GoodOnYouScrapper.Service/Utils/AppConstants.cs
@@ -23,30 +23,30 @@ public abstract class AppConstants
                                                           "Please use one of the new APIs available from the explorer.";
 
     /// <summary>Base URL to access GoodOnYou</summary>
-    public static readonly string WebSiteUrl = "https://directory.goodonyou.eco/brand/";
-    
+    public const string WebSiteUrl = "https://directory.goodonyou.eco/brand/";
+
     /// <summary>XPath for the environment rating in the brand page</summary>
-    public static readonly string XPathEnvironmentRating =
-        "//*[@id='__next']/div/div[4]/div/div[1]/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/span";
+    public const string XPathEnvironmentRating = "//*[@id='__next']/div/div[4]/div/div[1]/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/span";
+
     
     /// <summary>XPath for the people rating in the brand page</summary>
-    public static readonly string XPathPeopleRating =
-        "//*[@id='__next']/div/div[4]/div/div[1]/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div/span";
+    public const string XPathPeopleRating = "//*[@id='__next']/div/div[4]/div/div[1]/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div/span";
+
     
     /// <summary>XPath for the animal rating in the brand page</summary>
-    public static readonly string XPathAnimalRating =
+    public const string XPathAnimalRating = "//*[@id='__next']/div/div[4]/div/div[1]/div[2]/div[2]/div/div[1]/div[3]/div/div[2]/div/span";
         "//*[@id='__next']/div/div[4]/div/div[1]/div[2]/div[2]/div/div[1]/div[3]/div/div[2]/div/span";
 
     /// <summary>XPath for the rating description in the brand page</summary>
-    public static readonly string XPathRatingDescription =
+    public const string XPathRatingDescription = "//*[@id='rating-summary-text']";
         "//*[@id='rating-summary-text']";
 
     /// <summary>Regex to extract the rating from the rating description</summary>
-    public static readonly string XPathBrandCountry =
+    public const string XPathBrandCountry = "//*[@id='brand-hero-summary']/div/div[2]/p[3]";
         "//*[@id='brand-hero-summary']/div/div[2]/p[3]";
 
     /// <summary>XPath for sidebar</summary>
-    public static readonly string XPathSideBar =
+    public const string XPathSideBar = "//*[@id='sideBar']";
         "//*[@id='sideBar']";
     
 }

453fa9c0ad6ffbf8ebc789a21c26321b2cc0056e
Thomas Blondel <tfoenix@gmail.com>
2023-08-06 16:05:16 +0200

    [Refacto] Removed project BrandAndProductDatabase.API

--- BrandAndProductDatabase.API/DataAccess/IRepositories/IProductRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using BrandAndProductDatabase.API.Models.Dto;
-
-namespace BrandAndProductDatabase.API.DataAccess.IRepositories;
-
-/// <summary>Interface for ProductRepository</summary>
-public interface IProductRepository : IRepository<ProductDto>
-{
-}

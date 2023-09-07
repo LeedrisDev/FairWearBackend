e6faf35e1a0025f9de6941c3eed930b22ecdd9a6
Thomas Blondel <tfoenix@gmail.com>
2023-09-07 18:34:47 +0200

    [BO] Added docs on cs files

--- BackOffice/Pages/Error.cshtml.cs
+++ BackOffice/Pages/Error.cshtml.cs
@@ -1,26 +1,32 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BackOffice.Pages;
 
+/// <summary>PageModel class for handling errors.</summary>
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
+    /// <summary>Gets or sets the unique request identifier associated with the error.</summary>
     public string? RequestId { get; set; }
 
+    /// <summary>Gets a boolean value indicating whether the request identifier is available and not empty.</summary>
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     private readonly ILogger<ErrorModel> _logger;
 
+    /// <summary>Constructor to initialize the ErrorModel with a logger.</summary>
+    /// <param name="logger">The logger for the ErrorModel.</param>
     public ErrorModel(ILogger<ErrorModel> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>HTTP GET request handler for the error page.</summary>
     public void OnGet()
     {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        // This method is used for handling HTTP GET requests to the error page.
+        // It sets the RequestId based on the current Activity or TraceIdentifier in the HttpContext.
     }
 }
--- BackOffice/Pages/Privacy.cshtml.cs
+++ BackOffice/Pages/Privacy.cshtml.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BackOffice.Pages;
 
+/// <summary>PageModel class for displaying the privacy page.</summary>
 public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
 
+    /// <summary>Constructor to initialize the PrivacyModel with a logger.</summary>
+    /// <param name="logger">The logger for the PrivacyModel.</param>
     public PrivacyModel(ILogger<PrivacyModel> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>HTTP GET request handler for the privacy page.</summary>
     public void OnGet()
     {
+        // This method is used for handling HTTP GET requests to the privacy page.
     }
 }

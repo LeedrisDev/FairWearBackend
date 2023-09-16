a8e039da9db6a357b669a4e8b0f43c61d52ba2f2
Thomas Blondel <tfoenix@gmail.com>
2023-09-16 23:08:04 +0200

    [BO] Enabled authentication in program.cs

--- BackOffice/Program.cs
+++ BackOffice/Program.cs
@@ -20,11 +20,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();

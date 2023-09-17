a4667a2d57944ce77f329057ad45da0fdcdfea97
Thomas Blondel <tfoenix@gmail.com>
2023-09-17 23:05:53 +0200

    [BO] Cleaned logout.cshtml.cs

--- BackOffice/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BackOffice/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,43 +1,41 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using BackOffice.DataAccess.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace BackOffice.Areas.Identity.Pages.Account
+namespace BackOffice.Areas.Identity.Pages.Account;
+
+/// <summary>Razor page model for the logout page.</summary>
+public class LogoutModel : PageModel
 {
-    public class LogoutModel : PageModel
+    private readonly SignInManager<UserEntity> _signInManager;
+    private readonly ILogger<LogoutModel> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LogoutModel"/> class.
+    /// </summary>
+    /// <param name="signInManager">The sign-in manager for managing user sign-out operations.</param>
+    /// <param name="logger">The logger for logging user logout-related activities.</param>
+    public LogoutModel(SignInManager<UserEntity> signInManager, ILogger<LogoutModel> logger)
     {
-        private readonly SignInManager<UserEntity> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+        _signInManager = signInManager;
+        _logger = logger;
+    }
 
-        public LogoutModel(SignInManager<UserEntity> signInManager, ILogger<LogoutModel> logger)
-        {
-            _signInManager = signInManager;
-            _logger = logger;
-        }
+    /// <summary>
+    /// Handles the HTTP POST request for user logout.
+    /// </summary>
+    /// <param name="returnUrl">The URL to return to after the user logs out.</param>
+    /// <returns>An <see cref="IActionResult"/> representing the result of the logout operation.</returns>
+    public async Task<IActionResult> OnPost(string? returnUrl = null)
+    {
+        await _signInManager.SignOutAsync();
+        _logger.LogInformation("User logged out.");
+        if (returnUrl != null)
+            return LocalRedirect(returnUrl);
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
-        }
+        // This needs to be a redirect so that the browser performs a new
+        // request and the identity for the user gets updated.
+        return RedirectToPage();
     }
 }

4c4d8fc4e85bce758ca9f1ca79b3e0422b6f76ed
Thomas Blondel <tfoenix@gmail.com>
2023-09-20 14:51:49 +0200

    [BO Brands] Added FindByNameAsync method in brand repository

--- BackOffice/DataAccess/Repositories/Classes/BrandRepository.cs
+++ BackOffice/DataAccess/Repositories/Classes/BrandRepository.cs
@@ -1,7 +1,9 @@
+using System.Net;
 using AutoMapper;
 using BackOffice.DataAccess.Entities;
 using BackOffice.DataAccess.Repositories.Interfaces;
 using BackOffice.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackOffice.DataAccess.Repositories.Classes;
 
@@ -14,4 +16,34 @@ public class BrandRepository : Repository<BrandModel, BrandEntity>, IBrandReposi
     /// <param name="mapper">The mapper used for mapping between <see cref="BrandModel"/> and <see cref="BrandEntity"/>.</param>
     public BrandRepository(BrandAndProductDbContext context, ILogger<Repository<BrandModel, BrandEntity>> logger, IMapper mapper) 
         : base(context, logger, mapper) { }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<BrandModel>> FindByNameAsync(string name)
+    {
+        var response = new ProcessingStatusResponse<BrandModel>();
+
+        try
+        {
+            var entity = await Set
+                .AsNoTracking()
+                .FirstOrDefaultAsync(e => e.Name == name);
+
+            if (entity == null)
+            {
+                response.StatusCode = HttpStatusCode.NotFound;
+                response.Message = $"No brand with name '{name}' was found.";
+                return response;
+            }
+            
+            response.Entity = Mapper.Map<BrandModel>(entity);
+        }
+        catch (Exception e)
+        {
+            Logger.LogError("Error on database {Exception}", e.ToString());
+            response.StatusCode = HttpStatusCode.InternalServerError;
+            response.Message = "Internal server error.";
+        }
+        
+        return response;
+    }
 }
--- BackOffice/DataAccess/Repositories/Interfaces/IBrandRepository.cs
+++ BackOffice/DataAccess/Repositories/Interfaces/IBrandRepository.cs
@@ -3,4 +3,11 @@ using BackOffice.Models;
 namespace BackOffice.DataAccess.Repositories.Interfaces;
 
 /// <summary>Represents a repository interface for managing brand entities.</summary>
-public interface IBrandRepository : IRepository<BrandModel> { }
+public interface IBrandRepository : IRepository<BrandModel>
+{
+    /// <summary>Asynchronously finds a <see cref="BrandModel"/> entity by its name.</summary>
+    /// <param name="name">The name of the <see cref="BrandModel"/> entity to find.</param>
+    /// <returns>A processing status response with the found <see cref="BrandModel"/> entity.</returns>
+    Task<ProcessingStatusResponse<BrandModel>> FindByNameAsync(string name);
+
+}

b686087d0d3934a0fe3a78e35f98391076a25ba1
felixngo <felix.ngo@epita.fr>
2023-09-26 04:27:55 +0200

    [GRPC] Uncomment add grpc service

--- BrandAndProduct.Service/Program.cs
+++ BrandAndProduct.Service/Program.cs
@@ -1,4 +1,5 @@
 using BrandAndProduct.Service.Config;
+using BrandAndProduct.Service.Services;
 using BrandAndProduct.Service.Utils;
 using GoodOnYouScrapper.Service.Protos;
 using ProductDataRetriever.Service.Protos;
@@ -25,8 +26,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-// app.MapGrpcService<BrandService>();
-// app.MapGrpcService<ProductService>();
+app.MapGrpcService<BrandService>();
+app.MapGrpcService<ProductService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");

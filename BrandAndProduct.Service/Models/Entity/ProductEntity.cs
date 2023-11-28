7608fd9e87f5f268c0574f08e3362cb2b860cdf7
Thomas Blondel <tfoenix@gmail.com>
2023-11-28 12:48:34 +0100

    [Entities] Added Color field to Product

--- BrandAndProduct.Service/Models/Entity/ProductEntity.cs
+++ BrandAndProduct.Service/Models/Entity/ProductEntity.cs
@@ -3,6 +3,9 @@
 /// <summary>Class representing a Product in database.</summary>
 public class ProductEntity : IObjectWithId
 {
+    /// <summary>The Id of the Product.</summary>
+    public int Id { get; set; }
+    
     /// <summary>The UpcCode of the Product.</summary>
     public string UpcCode { get; set; } = null!;
 
@@ -10,7 +13,10 @@ public class ProductEntity : IObjectWithId
     public string Name { get; set; } = null!;
 
     /// <summary>The category of the Product.</summary>
-    public string? Category { get; set; }
+    public string Category { get; set; } = null!;
+    
+    /// <summary>The Color of the Product.</summary>
+    public string Color { get; set; } = null!;
 
     /// <summary>The Ranges of the Product.</summary>
     /// <remarks>TODO : For IEnumerable should use hasconversion in dbcontext </remarks>
@@ -21,7 +27,4 @@ public class ProductEntity : IObjectWithId
 
     /// <summary>The <see cref="BrandEntity"/> of the Product.</summary>
     public virtual BrandEntity BrandEntity { get; set; } = null!;
-
-    /// <summary>The Id of the Product.</summary>
-    public int Id { get; set; }
 }

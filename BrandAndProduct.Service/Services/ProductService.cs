059108a07f8c5e21d654fcb050408e0cde6a19b7
Thomas Blondel <tfoenix@gmail.com>
2023-11-28 13:02:32 +0100

    [Services] Added a function to ProductService to retrieve product alternatives

--- BrandAndProduct.Service/Services/ProductService.cs
+++ BrandAndProduct.Service/Services/ProductService.cs
@@ -8,29 +8,43 @@ using Grpc.Core;
 
 namespace BrandAndProduct.Service.Services;
 
+/// <summary>
+/// gRPC service for managing products.
+/// </summary>
 public class ProductService : Protos.ProductService.ProductServiceBase
 {
     private readonly ILogger<ProductService> _logger;
     private readonly IMapper _mapper;
     private readonly IProductBusiness _productBusiness;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProductService"/> class.
+    /// </summary>
+    /// <param name="productBusiness">The product business service.</param>
+    /// <param name="logger">The logger.</param>
+    /// <param name="mapper">The mapper.</param>
     public ProductService(IProductBusiness productBusiness, ILogger<ProductService> logger, IMapper mapper)
     {
         _productBusiness = productBusiness;
         _logger = logger;
         _mapper = mapper;
     }
-
+    
+    
+    /// <summary>
+    /// Retrieves all products based on specified filters.
+    /// </summary>
+    /// <param name="request">The request containing filters.</param>
+    /// <param name="responseStream">The stream for sending product responses.</param>
+    /// <param name="context">The server call context.</param>
     public override async Task GetAllProductsAsync(ProductFilterList request,
         IServerStreamWriter<ProductResponse> responseStream,
         ServerCallContext context)
     {
         var filters = new Dictionary<string, string>();
 
-        foreach (ProductFilter filter in request.Filters)
-        {
+        foreach (var filter in request.Filters)
             filters.Add(filter.Key, filter.Value);
-        }
 
         var productList = await _productBusiness.GetAllProductsAsync(filters);
 
@@ -40,12 +54,16 @@ public class ProductService : Protos.ProductService.ProductServiceBase
             throw new RpcException(new Status(StatusCode.Internal, productList.ErrorMessage));
         }
 
-        foreach (ProductDto product in productList.Object)
-        {
+        foreach (var product in productList.Object)
             await responseStream.WriteAsync(_mapper.Map<ProductResponse>(product));
-        }
     }
 
+    /// <summary>
+    /// Retrieves a product by its ID.
+    /// </summary>
+    /// <param name="request">The request containing the product ID.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>The product response.</returns>
     public override async Task<ProductResponse> GetProductByIdAsync(ProductByIdRequest request,
         ServerCallContext context)
     {
@@ -62,6 +80,12 @@ public class ProductService : Protos.ProductService.ProductServiceBase
         };
     }
 
+    /// <summary>
+    /// Retrieves product information by UPC code.
+    /// </summary>
+    /// <param name="request">The request containing the UPC code.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>The product information response.</returns>
     public override async Task<ProductInformationResponse> GetProductByUpcAsync(ProductByUpcRequest request,
         ServerCallContext context)
     {
@@ -78,6 +102,12 @@ public class ProductService : Protos.ProductService.ProductServiceBase
         };
     }
 
+    /// <summary>
+    /// Creates a new product.
+    /// </summary>
+    /// <param name="request">The request containing the product details.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>The created product response.</returns>
     public override async Task<ProductResponse> CreateProductAsync(ProductRequest request, ServerCallContext context)
     {
         var createdProduct = await _productBusiness.CreateProductAsync(_mapper.Map<ProductDto>(request));
@@ -92,6 +122,12 @@ public class ProductService : Protos.ProductService.ProductServiceBase
         };
     }
 
+    /// <summary>
+    /// Updates an existing product.
+    /// </summary>
+    /// <param name="request">The request containing the updated product details.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>The updated product response.</returns>
     public override async Task<ProductResponse> UpdateProductAsync(ProductResponse request, ServerCallContext context)
     {
         var updatedProduct = await _productBusiness.UpdateProductAsync(_mapper.Map<ProductDto>(request));
@@ -108,6 +144,12 @@ public class ProductService : Protos.ProductService.ProductServiceBase
         };
     }
 
+    /// <summary>
+    /// Deletes a product by its ID.
+    /// </summary>
+    /// <param name="request">The request containing the product ID.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>An empty response indicating the success of the operation.</returns>
     public override async Task<Empty> DeleteProductAsync(ProductByIdRequest request, ServerCallContext context)
     {
         var product = await _productBusiness.GetProductByIdAsync(request.Id);
@@ -132,5 +174,23 @@ public class ProductService : Protos.ProductService.ProductServiceBase
             HttpStatusCode.OK => new Empty(),
             _ => throw new RpcException(new Status(StatusCode.Internal, product.ErrorMessage))
         };
+    }
+
+    /// <summary>
+    /// Retrieves product alternatives based on the specified product ID.
+    /// </summary>
+    /// <param name="request">The request containing the product ID.</param>
+    /// <param name="responseStream">The stream for sending product responses.</param>
+    /// <param name="context">The server call context.</param>
+    public override async Task GetProductAlternativesAsync(ProductByIdRequest request, IServerStreamWriter<ProductResponse> responseStream,
+        ServerCallContext context)
+    {
+        var productAlternatives = await _productBusiness.GetProductAlternativesAsync(request.Id);
+
+        if (productAlternatives.Status != HttpStatusCode.OK)
+            _logger.LogError("Error while retrieving product alternatives: {ErrorMessage}", productAlternatives.ErrorMessage);
+
+        foreach (var product in productAlternatives.Object)
+            await responseStream.WriteAsync(_mapper.Map<ProductResponse>(product));
     }
 }

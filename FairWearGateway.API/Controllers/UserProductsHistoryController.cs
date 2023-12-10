af7a0d34e8901de464a8467ae8827554ced88f32
Thomas Blondel <tfoenix@gmail.com>
2023-12-10 15:25:59 +0100

    [Gateway] Added doc on UserProductsHistoryController

--- FairWearGateway.API/Controllers/UserProductsHistoryController.cs
+++ FairWearGateway.API/Controllers/UserProductsHistoryController.cs
@@ -9,6 +9,9 @@ using Users.Service;
 
 namespace FairWearGateway.API.Controllers;
 
+/// <summary>
+/// Controller for managing user product history.
+/// </summary>
 [ApiController]
 [Route("/api/history/")]
 [Produces("application/json")]
@@ -16,11 +19,20 @@ public class UserProductsHistoryController : ControllerBase
 {
     private readonly IUserProductHistoryBusiness _userProductHistoryBusiness;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserProductsHistoryController"/> class.
+    /// </summary>
+    /// <param name="userProductHistoryBusiness">The user product history business service.</param>
     public UserProductsHistoryController(IUserProductHistoryBusiness userProductHistoryBusiness)
     {
         _userProductHistoryBusiness = userProductHistoryBusiness;
     }
 
+    /// <summary>
+    /// Retrieves user product history by user ID.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <returns>The user product history response.</returns>
     [HttpGet("{userId:long}")]
     [ProducesResponseType(typeof(GetUserProductHistoryResponse), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.NotFound)]
@@ -37,6 +49,11 @@ public class UserProductsHistoryController : ControllerBase
         };
     }
 
+    /// <summary>
+    /// Creates user product history.
+    /// </summary>
+    /// <param name="request">The request to create user product history.</param>
+    /// <returns>The created user product history.</returns>
     [HttpPost()]
     [ProducesResponseType(typeof(User), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
@@ -48,7 +65,7 @@ public class UserProductsHistoryController : ControllerBase
             ProductId = request.ProductId,
             Timestamp = Timestamp.FromDateTime(DateTime.UtcNow.ToUniversalTime())
         };
-        
+
         var processingStatusResponse = _userProductHistoryBusiness.CreateUserProductHistory(userProductHistory);
 
         return processingStatusResponse.Status switch
@@ -58,6 +75,11 @@ public class UserProductsHistoryController : ControllerBase
         };
     }
 
+    /// <summary>
+    /// Updates user product history.
+    /// </summary>
+    /// <param name="request">The user product history details to update.</param>
+    /// <returns>The updated user product history.</returns>
     [HttpPut()]
     [ProducesResponseType(typeof(User), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.InternalServerError)]
@@ -72,7 +94,11 @@ public class UserProductsHistoryController : ControllerBase
         };
     }
 
-
+    /// <summary>
+    /// Deletes user product history by user ID.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <returns>The deleted user product history.</returns>
     [HttpDelete("{userId:long}")]
     [ProducesResponseType(typeof(UserProductHistory), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(UserProductHistory), (int)HttpStatusCode.NotFound)]

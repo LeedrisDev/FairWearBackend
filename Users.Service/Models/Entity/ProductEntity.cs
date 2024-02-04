7ff503bf0ede017e42dcd6b7ae34b7ac5af6915b
Thomas Blondel <tfoenix@gmail.com>
2024-02-05 00:04:58 +0100

    [US] Started to fix linter

--- Users.Service/Models/Entity/ProductEntity.cs
+++ Users.Service/Models/Entity/ProductEntity.cs
@@ -1,29 +1,28 @@
-﻿namespace Users.Service.Models.Entity
+﻿namespace Users.Service.Models.Entity;
+
+/// <summary>
+/// Represents a Product entity.
+/// </summary>
+public class ProductEntity : IObjectWithId
 {
     /// <summary>
-    /// Represents a Product entity.
+    /// Gets or sets the name of the product.
     /// </summary>
-    public partial class ProductEntity : IObjectWithId
-    {
-        /// <summary>
-        /// Gets or sets the name of the product.
-        /// </summary>
-        public string Name { get; set; } = null!;
+    public string Name { get; set; } = null!;
 
-        /// <summary>
-        /// Gets or sets the rating of the product.
-        /// </summary>
-        public int Rating { get; set; }
+    /// <summary>
+    /// Gets or sets the rating of the product.
+    /// </summary>
+    public int Rating { get; set; }
 
-        /// <summary>
-        /// Gets or sets the collection of user product histories associated with this product.
-        /// </summary>
-        public virtual ICollection<UserProductHistoryEntity> UserProductHistories { get; set; } =
-            new List<UserProductHistoryEntity>();
+    /// <summary>
+    /// Gets or sets the collection of user product histories associated with this product.
+    /// </summary>
+    public virtual ICollection<UserProductHistoryEntity> UserProductHistories { get; set; } =
+        new List<UserProductHistoryEntity>();
 
-        /// <summary>
-        /// Gets or sets the unique identifier for the product.
-        /// </summary>
-        public long Id { get; set; }
-    }
+    /// <summary>
+    /// Gets or sets the unique identifier for the product.
+    /// </summary>
+    public long Id { get; set; }
 }

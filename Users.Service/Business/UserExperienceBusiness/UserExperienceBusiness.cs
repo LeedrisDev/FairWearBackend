529edd6fb707ea8c174ea6d2309f7611ffb9f4a3
Thomas Blondel <tfoenix@gmail.com>
2024-02-05 00:11:26 +0100

    [US] Continue fixing linter

--- Users.Service/Business/UserExperienceBusiness/UserExperienceBusiness.cs
+++ Users.Service/Business/UserExperienceBusiness/UserExperienceBusiness.cs
@@ -4,86 +4,84 @@ using Users.Service.DataAccess.IRepositories;
 using Users.Service.Models;
 using Users.Service.Models.Dto;
 
-namespace Users.Service.Business.UserExperienceBusiness
+namespace Users.Service.Business.UserExperienceBusiness;
+
+/// <summary>Business logic for userExperiences database actions </summary>
+public class UserExperienceBusiness : IUserExperienceBusiness
 {
-    /// <summary>Business logic for userExperiences database actions </summary>
-    public class UserExperienceBusiness : IUserExperienceBusiness
+    private readonly IFilterFactory<IFilter> _filterFactory;
+    private readonly IUserExperienceRepository _userExperienceRepository;
+
+    /// <summary>
+    /// Constructor for UserExperienceBusiness.
+    /// </summary>
+    /// <param name="userExperienceRepository"></param>
+    /// <param name="filterFactory"></param>
+    public UserExperienceBusiness(IUserExperienceRepository userExperienceRepository,
+        IFilterFactory<IFilter> filterFactory)
     {
-        private readonly IFilterFactory<IFilter> _filterFactory;
-        private readonly IUserExperienceRepository _userExperienceRepository;
+        _userExperienceRepository = userExperienceRepository;
+        _filterFactory = filterFactory;
+    }
 
-        /// <summary>
-        /// Constructor for UserExperienceBusiness.
-        /// </summary>
-        /// <param name="userExperienceRepository"></param>
-        /// <param name="filterFactory"></param>
-        public UserExperienceBusiness(IUserExperienceRepository userExperienceRepository,
-            IFilterFactory<IFilter> filterFactory)
+    /// <inheritdoc/>
+    public Task<ProcessingStatusResponse<IEnumerable<UserExperienceDto>>> GetAllUserExperiencesAsync(
+        Dictionary<string, string> filters)
+    {
+        var filter = _filterFactory.CreateFilter(filters);
+        return _userExperienceRepository.GetAllAsync(filter);
+    }
+
+    /// <inheritdoc/>
+    public Task<ProcessingStatusResponse<UserExperienceDto>> GetUserExperienceByIdAsync(long id)
+    {
+        return _userExperienceRepository.GetByIdAsync(id);
+    }
+
+    /// <inheritdoc/>
+    public async Task<ProcessingStatusResponse<UserExperienceDto>> GetUserExperienceByUserIdAsync(long id)
+    {
+        var processingStatusResponse = new ProcessingStatusResponse<UserExperienceDto>();
+
+        var dict = new Dictionary<string, string>()
         {
-            _userExperienceRepository = userExperienceRepository;
-            _filterFactory = filterFactory;
+            { "UserId", id.ToString() }
+        };
+
+        var filter = _filterFactory.CreateFilter(dict);
+
+        var results = await _userExperienceRepository.GetAllAsync(filter);
+
+        if (!results.Object.Any())
+        {
+            processingStatusResponse.Status = HttpStatusCode.NotFound;
+            processingStatusResponse.ErrorMessage = $"UserExperience with userId {id} not found.";
+        }
+        else
+        {
+            processingStatusResponse.Object = results.Object.First();
         }
 
-        /// <inheritdoc/>
-        public async Task<ProcessingStatusResponse<IEnumerable<UserExperienceDto>>> GetAllUserExperiencesAsync(
-            Dictionary<string, string> filters)
-        {
-            var filter = _filterFactory.CreateFilter(filters);
+        return processingStatusResponse;
+    }
 
-            return await _userExperienceRepository.GetAllAsync(filter);
-        }
+    /// <inheritdoc/>
+    public Task<ProcessingStatusResponse<UserExperienceDto>> CreateUserExperienceAsync(
+        UserExperienceDto userExperienceDto)
+    {
+        return _userExperienceRepository.AddAsync(userExperienceDto);
+    }
 
-        /// <inheritdoc/>
-        public async Task<ProcessingStatusResponse<UserExperienceDto>> GetUserExperienceByIdAsync(long id)
-        {
-            return await _userExperienceRepository.GetByIdAsync(id);
-        }
+    /// <inheritdoc/>
+    public Task<ProcessingStatusResponse<UserExperienceDto>> UpdateUserExperienceAsync(
+        UserExperienceDto userExperienceDto)
+    {
+        return _userExperienceRepository.UpdateAsync(userExperienceDto);
+    }
 
-        /// <inheritdoc/>
-        public async Task<ProcessingStatusResponse<UserExperienceDto>> GetUserExperienceByUserIdAsync(long id)
-        {
-            var processingStatusResponse = new ProcessingStatusResponse<UserExperienceDto>();
-
-            var dict = new Dictionary<string, string>()
-            {
-                { "UserId", id.ToString() }
-            };
-
-            var filter = _filterFactory.CreateFilter(dict);
-
-            var results = await _userExperienceRepository.GetAllAsync(filter);
-
-            if (!results.Object.Any())
-            {
-                processingStatusResponse.Status = HttpStatusCode.NotFound;
-                processingStatusResponse.ErrorMessage = $"UserExperience with userId {id} not found.";
-            }
-            else
-            {
-                processingStatusResponse.Object = results.Object.First();
-            }
-
-            return processingStatusResponse;
-        }
-
-        /// <inheritdoc/>
-        public async Task<ProcessingStatusResponse<UserExperienceDto>> CreateUserExperienceAsync(
-            UserExperienceDto userExperienceDto)
-        {
-            return await _userExperienceRepository.AddAsync(userExperienceDto);
-        }
-
-        /// <inheritdoc/>
-        public async Task<ProcessingStatusResponse<UserExperienceDto>> UpdateUserExperienceAsync(
-            UserExperienceDto userExperienceDto)
-        {
-            return await _userExperienceRepository.UpdateAsync(userExperienceDto);
-        }
-
-        /// <inheritdoc/>
-        public async Task<ProcessingStatusResponse<UserExperienceDto>> DeleteUserExperienceAsync(long id)
-        {
-            return await _userExperienceRepository.DeleteAsync(id);
-        }
+    /// <inheritdoc/>
+    public Task<ProcessingStatusResponse<UserExperienceDto>> DeleteUserExperienceAsync(long id)
+    {
+        return _userExperienceRepository.DeleteAsync(id);
     }
 }
